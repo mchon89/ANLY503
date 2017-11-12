dc3cca0bc632e8bf44d9b33ff70cd931c908d51a
Michael Chon <mchon89@gmail.com>
2017-11-12 13:32:42 -0500

    Update ggplot2_2.R

--- ggplot2/2/ggplot2_2.R
+++ ggplot2/2/ggplot2_2.R
@@ -1,15 +1,25 @@
+# Michael Chon
+# ANLY503 Portfolio
+# ggplot2
+
 library(ggplot2)
 library(ggthemes)
 
-
+# Reading the file 
 data = read.csv('language.csv')
+
+# Collecting the column needed for this visual
 var <- data$parent
 
+# Creating the table for a waffle chart
 nrows <- 10
 df <- expand.grid(y = 1:nrows, x = 1:nrows)
 categ_table <- round(table(var) * ((nrows*nrows)/(length(var))))
+
+# Adding category
 df$category <- factor(rep(names(categ_table), categ_table))  
 
+# Plotting a waffle chart 
 ggplot2 <- ggplot(df, aes(x = x, y = y, fill = category)) + geom_tile(color = "black", size = 0.5)
 ggplot2 + scale_x_continuous(expand = c(0, 0)) +
   scale_y_continuous(expand = c(0, 0), trans = 'reverse') +
