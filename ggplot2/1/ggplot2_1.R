ff5b779945225fd2c324ee2bb5f08f4b8c0f248b
Michael Chon <mchon89@gmail.com>
2017-11-12 13:30:16 -0500

    Update ggplot2_1.R

--- ggplot2/1/ggplot2_1.R
+++ ggplot2/1/ggplot2_1.R
@@ -1,6 +1,9 @@
 library(ggplot2)
 library(ggthemes)
 
+# Reading the csv file
 data = read.csv('burden.csv')
+
+# Plotting using the ggplot2 and economist ggtheme
 ggplot_1 <- ggplot(data, aes(x=Burden, y=Value, fill=Benefit)) + geom_boxplot()
 ggplot_1 + theme_economist() + scale_colour_economist() + ggtitle('Mutation Burden vs Neoantigen Burden')
