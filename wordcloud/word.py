d707298b6089c55262f3e5b384139789de17978a
Michael Chon <mchon89@gmail.com>
2017-11-20 20:28:10 -0500

    Update word.py

--- wordcloud/word.py
+++ wordcloud/word.py
@@ -9,31 +9,36 @@ import matplotlib.pyplot as plt
 
 from wordcloud import WordCloud, STOPWORDS
 
-
+# Current path
 d = path.dirname(__file__)
 
+# The list of the novels
 jane = ['Austen_Emma.txt', 'Austen_Pride.txt', 'Austen_Sense.txt']
 
+# Reading Emma first
 emma = open('Austen_Emma.txt', 'r')
 text = emma.read()
 
+# Combining the rest
 for i in range(1,3):
 	temp = open(jane[i], 'r')
 	text = text + temp.read()
 
+# Reading the Jane_mask1.jpg into numpy array
 jane_mask = np.array(Image.open(path.join(d, "jane_mask1.jpg")))
 
+# Stopwords
 stopwords = set(STOPWORDS)
 stopwords.add("said")
 
+# Generate word cloud based on the numpy array
 wc = WordCloud(background_color="white", max_words=2000, mask=jane_mask, stopwords=stopwords)
-# generate word cloud
 word = wc.generate(text)
 
-# store to file
+# Storing the file
 word.to_file(path.join(d, "alice.png"))
 
-# show
+# Showing
 fig = plt.figure(figsize=(8,6))
 plt.imshow(wc, interpolation='bilinear')
 plt.axis("off")
